69966ccb7e6461d9e17ab0f796f6c9d83fdb3675
Shad <hopson.shad@gmail.com>
2018-09-01 15:19:30 -0400

    Initial commit

---
+++ birds.js
@@ -0,0 +1,237 @@
+birds = {
+"Northern Mockingbird": ["http://68.media.tumblr.com/3852ac9b92808de4a570a89590624ca4/tumblr_on9opg31gU1u3vbwdo1_1280.jpg"],
+"American Robin": ["http://68.media.tumblr.com/7f1efdb1dbf5445d0e21d620c9344301/tumblr_on60pegeLg1u3vbwdo2_r1_1280.jpg"],
+"Ring-billed Gull": ["http://68.media.tumblr.com/8bafb255e9911c470af535f7793988b6/tumblr_on3fpyvGau1u3vbwdo1_1280.jpg"],
+"Downy Woodpecker": ["http://68.media.tumblr.com/67614fd9f52a664fd5806b2c91e313dd/tumblr_omvxgzJcaL1u3vbwdo1_1280.jpg"],
+"Killdeer" : ["http://68.media.tumblr.com/f7a9ead87527493287e36f06634b6862/tumblr_omq3y6AEWa1u3vbwdo1_1280.jpg"],
+"Common Grackle" : ["http://68.media.tumblr.com/e9f120407fe7e4e63bc27c95b7084b7a/tumblr_onnn0lW6SX1u3vbwdo1_540.jpg"],
+"Eastern Phoebe" : ["http://68.media.tumblr.com/c8aae5255fb76fca10ffd85132112c37/tumblr_ong3kuGPwX1u3vbwdo1_1280.jpg"],
+"Song Sparrow" : ["http://68.media.tumblr.com/f4ed3a3b3c5a843393bd9469644328c7/tumblr_omlnt4lAjj1u3vbwdo1_1280.jpg"],
+"Brant" : ["http://68.media.tumblr.com/ddb493fd7b39c3687183860d35950bd3/tumblr_omf9tko9Ql1u3vbwdo1_1280.jpg"],
+"Great Black-backed Gull" : ["http://68.media.tumblr.com/60a94907c78849fd385aa5602f6d31e2/tumblr_om8ppyNIFv1u3vbwdo1_1280.jpg"],
+"Mourning Dove": ["http://68.media.tumblr.com/2e44a0a1113e91fe9969f69160cbabe9/tumblr_om337nBOYQ1u3vbwdo1_1280.jpg"],
+"Red-breasted Merganser" : ["http://68.media.tumblr.com/d4557b6763129b8f3a849eef5cce90cb/tumblr_ols1ohmFRL1u3vbwdo1_1280.jpg"],
+"White-throated Sparrow" : ["http://68.media.tumblr.com/ee9f07c296dc3122f7a702716738e826/tumblr_ol24obGxBI1u3vbwdo1_1280.jpg"],
+"Rock Pidgeon" : ["http://68.media.tumblr.com/1e82a4bbe64a8cae5ccc37137125fd5f/tumblr_okvujtkvly1u3vbwdo1_1280.jpg"],
+"Swamp Sparrow" : ["http://68.media.tumblr.com/9d788d31ab8cbe83127c1be394a4efea/tumblr_ok8hxnOHE41u3vbwdo1_1280.jpg"],
+"Bufflehead" : ["http://68.media.tumblr.com/cb0e64fd06feb92d7f6caec5875f7b44/tumblr_ok0gyynrfN1u3vbwdo1_1280.jpg"],
+"Double-crested Cormorant" : ["http://68.media.tumblr.com/3df190555f47441120897a1106121a1f/tumblr_ojwhnnDWhn1u3vbwdo1_1280.jpg"],
+"Mallard" : ["http://68.media.tumblr.com/bb278c2806afec8432ac873d55787f83/tumblr_oju4f7aGT21u3vbwdo1_1280.jpg"],
+"Northern Cardinal" : ["http://68.media.tumblr.com/18e9427d380a466b90d7115e355d68a2/tumblr_ojhc9j3sMc1u3vbwdo1_1280.jpg"],
+"Dark-eyed Junco" : ["http://68.media.tumblr.com/f60873176fdd59fb33f976d54bd40703/tumblr_oj5qalcRzU1u3vbwdo2_r1_1280.jpg"],
+"Red-tailed Hawk" : ["http://68.media.tumblr.com/aa322558349cecebc2e08cb4a5ade5e9/tumblr_oi1q4qEqx21u3vbwdo1_1280.jpg"],
+"Common Raven" : ["http://68.media.tumblr.com/990d6c66a762e237256ef41f5c3f8f43/tumblr_ohn07x4uOu1u3vbwdo1_1280.jpg"],
+"American Black Duck" : ["http://68.media.tumblr.com/daf842f5e5656aaf905e581081ced344/tumblr_ohcrw2K2bA1u3vbwdo1_1280.jpg"],
+"American Goldfinch": ["http://68.media.tumblr.com/20aa21b7dd965ff567cbc570b350c166/tumblr_ogzudr0biT1u3vbwdo1_1280.jpg"],
+"Fox Sparrow" : ["http://68.media.tumblr.com/7516f9348e7d71bfd986ef8a1dbde821/tumblr_ogqm6wjIsI1u3vbwdo1_1280.jpg"],
+"American Crow" : ["http://68.media.tumblr.com/25a27550399881649bec7336c485b35d/tumblr_nzidg67L1t1u3vbwdo2_r1_1280.jpg"],
+"Gadwall" : ["http://68.media.tumblr.com/3bb9500ab962ddc207780b0db4307976/tumblr_ogdmvv8qkZ1u3vbwdo1_1280.jpg"],
+"Red-breasted Nuthatch" : ["http://68.media.tumblr.com/58dbc4f4003f506433d1c3ff2a30df36/tumblr_og8xdxlkCm1u3vbwdo1_1280.jpg"],
+"Belted Kingfisher" : ["http://68.media.tumblr.com/68195c7700e0f9e5dad6446d48237dfe/tumblr_og8wwoybDV1u3vbwdo1_1280.jpg"],
+"Chipping Sparrow" : ["http://68.media.tumblr.com/bf90a663eefa01253c218b6a78cfd039/tumblr_ofyq03oaDy1u3vbwdo1_1280.jpg"],
+"Swainson's Thrush" : ["http://68.media.tumblr.com/ebb5ac45f16b721afc5f0f481ac6a7fe/tumblr_ofnlclwoZS1u3vbwdo1_1280.jpg"],
+"Black-capped Chickadee" : ["http://68.media.tumblr.com/6a14374e57f41639d8157c5a70bf507d/tumblr_ofjwc2OaJf1u3vbwdo1_1280.jpg"],
+"House Finch" : ["http://68.media.tumblr.com/d16265e5c44af87b49b7fe7baa2ab60f/tumblr_ofch50CBFG1u3vbwdo1_1280.jpg"],
+"Brown Creeper" : ["http://68.media.tumblr.com/a79a7a45c9d7c098dd4ea034a16a52c0/tumblr_ofam8rNiM61u3vbwdo1_1280.jpg"],
+"Palm Warbler" : ["http://68.media.tumblr.com/ed95867b82e9eee06d1fedd4cb8737b2/tumblr_of8twz7CcW1u3vbwdo1_1280.jpg"],
+"White-crowned Sparrow" : ["http://68.media.tumblr.com/a0655af2e0db439c67cb7e6315c9c0dd/tumblr_of6wjikXQz1u3vbwdo1_1280.jpg"],
+"Yellow-rumped Warbler" : ["http://68.media.tumblr.com/07da603ae1ba2b2672ba69443b5bc2cb/tumblr_of1dgt40iN1u3vbwdo1_1280.jpg"],
+"Blue-headed Vireo":["http://68.media.tumblr.com/843d037ad8fade9b747662c53d68c352/tumblr_oexo5qeDRj1u3vbwdo1_1280.jpg"],
+"Ruby-crowned Kinglet":["http://68.media.tumblr.com/b76c203b14de9013335edda51de5c90c/tumblr_oeoefdLkKV1u3vbwdo1_1280.jpg"],
+"Common Yellowthroat" : [ "http://68.media.tumblr.com/cb4224f4b91a46d7e11f4548b2b33ddb/tumblr_oekpjpeuGb1u3vbwdo1_1280.jpg"],
+"Marsh Wren": ["http://68.media.tumblr.com/5b393ca93c7271f1d550dc64e9a1dc9f/tumblr_oegzhzd0mS1u3vbwdo1_1280.jpg"],
+"Blackpoll Warbler": ["http://68.media.tumblr.com/469bac66676b548c5594d1bbb4b04f80/tumblr_oebi9k9sHF1u3vbwdo1_1280.jpg"],
+"Northern Parula": ["http://68.media.tumblr.com/18eff3061a68a91b1d6f329f02e15d58/tumblr_oe7tiuG7df1u3vbwdo1_1280.jpg"],
+"Yellow Warbler" : ["http://68.media.tumblr.com/6315a3b34db111b60bd24ac02e32d05f/tumblr_oe41g348gE1u3vbwdo1_1280.jpg"],
+"Northern Flicker" : ["http://68.media.tumblr.com/2a0534369f5206d137208f8cc633d565/tumblr_oe2nbeW1BO1u3vbwdo1_1280.jpg"],
+"Northern Waterthrush" : ["http://68.media.tumblr.com/bec087c6790ceba2ccf07306ce0f140d/tumblr_odliv5iF4R1u3vbwdo1_1280.jpg"],
+"Black-and-White Warbler (female)" : ["http://68.media.tumblr.com/f184f0043731c2a91a1ceb81c74a92a7/tumblr_oddaqpjudr1u3vbwdo1_1280.jpg"],
+"Magnolia Warbler (female)" : ["http://68.media.tumblr.com/e90165b09af72bfb70e46ccba063cfa1/tumblr_od31ibqKuF1u3vbwdo1_1280.jpg"],
+"Gray Catbird" : ["http://68.media.tumblr.com/868fe9e8fda71a6f1359187aadbfa44f/tumblr_ocgwvy5kYU1u3vbwdo1_1280.jpg"],
+"Laughing Gull" : ["http://68.media.tumblr.com/e9bf958d08252ce2a651c0de442cd083/tumblr_oc5nj4nuT91u3vbwdo1_1280.jpg"],
+"Barn Swallow" : ["http://68.media.tumblr.com/4ece7bbedfddb618897b33d820b1c7c1/tumblr_obsq3zW5ZZ1u3vbwdo1_1280.jpg"],
+"Common Tern" : ["http://68.media.tumblr.com/84aabf970bc4722c53a560d4e9fbf0c4/tumblr_obp219yV451u3vbwdo1_1280.jpg"],
+"Red-winged Blackbird" : ["http://68.media.tumblr.com/25a7682fc645a77a65b838e9a48800e8/tumblr_oa42vsha2e1u3vbwdo1_1280.jpg"],
+"Fish Crow" : ["http://68.media.tumblr.com/96f64ae7c7361d5a4884462e4a57e4a7/tumblr_o9y1nxxEZO1u3vbwdo1_1280.jpg"],
+"Cedar Waxwing" : ["http://68.media.tumblr.com/c6c36f10d2777639d246f7c7861770ca/tumblr_o92ixyZAk21u3vbwdo2_1280.jpg"],
+"Eastern Wood-Pewee" : ["http://68.media.tumblr.com/f65fa56fb1c3ff3c9434c94481f3eba9/tumblr_o7zq69PKBx1u3vbwdo1_1280.jpg"],
+"Red-eyed Vireo" : ["http://68.media.tumblr.com/d0781d87c6e6a9235a04899a3063cb87/tumblr_o7y1pj5BXi1u3vbwdo1_1280.jpg"],
+"American Redstart" : ["http://68.media.tumblr.com/e992891e5a66bcabbced94b8ff9ca2fb/tumblr_o7sb2qMfUY1u3vbwdo1_1280.jpg"],
+"Black-crowned Night-Heron" : ["http://68.media.tumblr.com/afea9006c73e537ecb37bdd16e1e402e/tumblr_o7lz6lwKwj1u3vbwdo1_1280.jpg"],
+"Canada Goose" : ["http://68.media.tumblr.com/642c7396f052b243617ba2b3c70e7d65/tumblr_o7h4q8JhZd1u3vbwdo1_1280.jpg"],
+"Chestnut-sided Warbler" : ["http://68.media.tumblr.com/bac6fbdfa831ca3bf73fc082404d27b9/tumblr_o7bjqb45Jl1u3vbwdo1_1280.jpg"],
+"Ovenbird" : ["http://68.media.tumblr.com/c6baf07bac5f58b8afd2bc2b23861fe7/tumblr_o6ym9nikeu1u3vbwdo1_1280.jpg"],
+"Spotted Sandpiper" : ["http://68.media.tumblr.com/1ad81b589affd1b79699fa9885e51fe4/tumblr_o6wu9fwmpb1u3vbwdo2_r1_1280.jpg"],
+"Savannah Sparrow" : ["http://68.media.tumblr.com/e529510891899dabdb84dab91d417d08/tumblr_o5xm5kJhNG1u3vbwdo1_1280.jpg"],
+"Yellow-bellied Sapsucker" : ["http://68.media.tumblr.com/c7d1c00f39f2743dbe594ef5415a75aa/tumblr_o5oc0rc0Ak1u3vbwdo1_1280.jpg"],
+"Lesser Scaup" : ["http://68.media.tumblr.com/c6850808575a416a8e25e3518fee4c6c/tumblr_o2hutqgVT41u3vbwdo2_r1_1280.jpg"],
+"Canvasback" : ["http://68.media.tumblr.com/6f5319a0c160d34c6d749eb14697339e/tumblr_o1d1x1qiFe1u3vbwdo1_1280.jpg"],
+"European Starling" : ["http://68.media.tumblr.com/b02415ba9df041c108d92ba23a705c14/tumblr_o0l9v7319E1u3vbwdo1_1280.jpg"],
+"Peregrine Falcon" : ["http://68.media.tumblr.com/ab8580803753f481012e5639fc4cbb3c/tumblr_nzeplgO0cT1u3vbwdo1_1280.gif"],
+"Wood Duck (female)" : ["http://68.media.tumblr.com/cba8d8fdd6dcd0e308fd7fb4a2756d90/tumblr_nwwlvuD5wB1u3vbwdo1_1280.jpg"],
+"Hermit Thrush" : ["http://68.media.tumblr.com/d9a7d30c6aab6b637c8201bf2be8d88a/tumblr_nwu1osa8bT1u3vbwdo1_1280.jpg"],
+"Tundra Swan" : ["http://68.media.tumblr.com/2791e494f96b976f0d86c6ebaa3ab255/tumblr_nwh4skjFbg1u3vbwdo1_1280.jpg"],
+"Eastern Towhee" : ["http://68.media.tumblr.com/e0993fa270b33b442f21f081a1638d1e/tumblr_nw9y172QxN1u3vbwdo1_1280.gif"],
+"Pine Warbler" : ["http://68.media.tumblr.com/9f28e6110ae1d5dbb93177492833a0cc/tumblr_nw5rxkVvbc1u3vbwdo1_1280.jpg"],
+"Sora" : ["http://68.media.tumblr.com/9623c7251d99ebf103c4ed409c66ba29/tumblr_nw0i4egQFJ1u3vbwdo1_1280.jpg"],
+"Black-throated Green Warbler" : ["http://68.media.tumblr.com/bf685b28ee30c8676ae66c43d07d7b3b/tumblr_nvk3zgxukt1u3vbwdo1_1280.jpg"],
+"Brown Thrasher" : ["http://68.media.tumblr.com/bcd58681bab84715ff4562904c113843/tumblr_nvg4lrjIvI1u3vbwdo1_1280.jpg"],
+"Wilson's Warbler" : ["http://68.media.tumblr.com/5adce053686f992c406edd28418e5fe0/tumblr_nve6bszIh51u3vbwdo1_1280.jpg"],
+"Lark Sparrow" : ["http://68.media.tumblr.com/effcae6a8849ecd40d90509dc97fd112/tumblr_nvakn6RVx91u3vbwdo1_1280.jpg"],
+"Rose-breasted Grosbeak" : ["http://68.media.tumblr.com/bfb4243b7bbcd1d8aec9affda2e5ac6d/tumblr_nuu0okFoZ01u3vbwdo1_1280.jpg"],
+"American Redstart" : ["http://68.media.tumblr.com/072c1e474ab52262cd2f48958558ad56/tumblr_nu25keecpY1u3vbwdo1_1280.jpg"],
+"Eastern Kingbird" : ["http://68.media.tumblr.com/d0678fcee9e1dbe269e2515aea27620f/tumblr_ntpatuIbk41u3vbwdo1_1280.jpg"],
+"Caronlina Wren" : ["http://68.media.tumblr.com/ab8a4dfc01fa7cf91ace0f8794361f8a/tumblr_ntn268Uumh1u3vbwdo1_1280.jpg"],
+"Blue-winged Warbler" : ["http://68.media.tumblr.com/8c34f72d84fe3a19fae91309c45cbd25/tumblr_ntdzum3H9q1u3vbwdo1_1280.jpg"],
+"Great Egret" : ["http://68.media.tumblr.com/3a32317745576438e912213595462d91/tumblr_nrfm2qlgtr1u3vbwdo1_1280.jpg"],
+"Greater Scaup" : ["http://68.media.tumblr.com/763a0859ea8cec348fc3f8d039aa7206/tumblr_npmu3jK3IW1u3vbwdo1_1280.jpg"],
+"Green Heron" : ["http://68.media.tumblr.com/f038b676792b41892c78a53f6d208812/tumblr_np0oowtx7i1u3vbwdo1_1280.jpg"],
+"Baltimore Oriole" : ["http://68.media.tumblr.com/5afed6bf8817d4750b88eb42014243d2/tumblr_nok4whZi6q1u3vbwdo1_1280.jpg"],
+"Field Sparrow" : ["http://68.media.tumblr.com/34f13909aa2b232d12ab44ce840f8a1d/tumblr_nniph35eyh1u3vbwdo1_1280.jpg"],
+"Red-throated Loon" : ["http://68.media.tumblr.com/736889c150f80ec10a022c5a052031ee/tumblr_njvhxl1Ap71u3vbwdo1_1280.jpg"],
+"Northern Shoveler" : ["http://68.media.tumblr.com/e4c87d21a72197a459e9823d5aa0128a/tumblr_ni8ou1IlLX1u3vbwdo1_1280.png"],
+"House Sparrow" : ["http://68.media.tumblr.com/107256f27be7510dd7f4049625748851/tumblr_o3extcW73d1u3vbwdo1_1280.jpg"],
+
+}
+
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+	return a;
+}
+
+
+var wrong = false;
+var img_num = 0;
+var keys = [];
+//get list of all tree names
+for (var key in birds) {
+	keys.push(key);
+}
+var points_per_problem = 100.0 / keys.length;
+var score = 0;
+var remaining_keys = keys.slice();//all keys minus the one we're currently using
+var keys_left = keys.slice(); //keys we haven't answered a question about
+
+var random_number = Math.floor(Math.random()*keys.length)
+var plant_choice = keys[random_number];
+
+var elem = document.createElement("img");
+
+var p_elem = document.createElement("h3");
+p_elem.setAttribute("id", "first");
+p_elem.innerHTML = plant_choice;
+remaining_keys.splice(random_number, 1);
+
+var random_number_2 = Math.floor(Math.random()*remaining_keys.length);
+var plant_choice_2 = remaining_keys[random_number_2];
+var p_elem_2 = document.createElement("h3");
+p_elem_2.setAttribute("id", "second");
+p_elem_2.innerHTML = plant_choice_2;
+remaining_keys.splice(random_number_2, 1);
+
+var random_number_3 = Math.floor(Math.random()*remaining_keys.length);
+var plant_choice_3 = remaining_keys[random_number_3];
+var p_elem_3 = document.createElement("h3");
+p_elem_3.setAttribute("id", "third");
+p_elem_3.innerHTML = plant_choice_3;
+
+//shuffles, images, chooses first one and adds it to site
+elem.src = shuffle(birds[plant_choice])[img_num];
+elem.setAttribute("height", "400");
+elem.setAttribute("width", "400");
+document.getElementById("tree").appendChild(elem);
+
+
+p_elem_li = [p_elem, p_elem_2, p_elem_3];
+shuffle(p_elem_li);
+document.getElementById("choices").appendChild(p_elem_li[0]);
+document.getElementById("choices").appendChild(p_elem_li[1]);
+document.getElementById("choices").appendChild(p_elem_li[2]);
+
+//document.getElementById("tree").onclick = function() { 
+//	if(keys_left.length > 0){
+//	if (img_num == 3) {
+//		img_num = 0;
+//	}
+//
+//	else {
+//		img_num++; 
+//	}
+//	elem.src = bird[plant_choice][img_num];
+//	}
+//};
+
+var p_elems = document.getElementsByTagName("h3");
+console.log(p_elems);
+console.log(p_elems[0].innerHTML);
+
+for( var i = 0; i <p_elems.length;i++){
+	p_elems[i].onclick = function() { 
+		if (keys_left.length > 1){	
+		if( this.innerHTML == plant_choice ) {
+			if (wrong) {
+				wrong = false;
+
+			}
+			else{
+				score += points_per_problem;
+			}
+			keys_left.splice(random_number,1);
+			random_number = Math.floor(Math.random()*keys_left.length)
+			plant_choice = keys_left[random_number];
+			shuffle(birds[plant_choice]);
+			p_elem.innerHTML = plant_choice;
+		
+			remaining_keys = keys.slice();
+			remove_index = remaining_keys.indexOf(plant_choice);
+			remaining_keys.splice(remove_index, 1);
+			shuffle(remaining_keys);
+			p_elem_2.innerHTML = remaining_keys[0];
+			p_elem_3.innerHTML = remaining_keys[1];
+		
+			shuffle(p_elem_li);
+			while (document.getElementById("choices").hasChildNodes()){
+				document.getElementById("choices").removeChild(document.getElementById("choices").lastChild);
+			}
+			for (i =0; i < p_elem_li.length; i++) {
+				document.getElementById("choices").appendChild(p_elem_li[i]);
+				p_elem_li[i].style.backgroundColor = "lightblue";
+				p_elem_li[i].addEventListener("mouseover", function( event ) {   
+    					// highlight the mouseover target
+    					event.target.style.backgroundColor = "#f9d11d";
+
+    // reset the color after a short delay
+    setTimeout(function() {
+      event.target.style.backgroundColor = "";
+    }, 100);
+  }, false);
+				//p_elem_li[i].onmouseover = function() { var the_choices = documents.getElementsByTagName("h3"); for (var i =0; i < the_choices.length; i++) { choices[i].style.backgroundColor = "red";}};
+			}
+			elem.src = birds[plant_choice][img_num];
+			
+		}
+		else{
+			this.style.backgroundColor = "red";
+			wrong = true;
+		}
+}
+		else{
+			console.log(score);
+			if(!wrong){
+				score += points_per_problem;
+				wrong = true;
+			}
+			document.getElementById("question").innerHTML = "You scored a " + String(Math.round(score)) + "!";
+			document.getElementById("question").style.color = "#f9d11d";
+		}
+	
+	};
+}
+
